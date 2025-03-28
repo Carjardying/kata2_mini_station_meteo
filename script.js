bb33223359500cbd89c9d24eca718b8012e6355e
Carjardying <carolinejardy.pro@gmail.com>
2025-03-28 19:01:18 +0100

    HORS EVALUATION : urlInput et .value dans la fonction donc ça marche

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-//Pardon d'avance ça ressemble à rien du toutparce que j'ai testé des trucs
+//Pardon d'avance ça ressemble à rien du tout parce que j'ai testé des trucs
 
 const cityInput = document.getElementById("cityInput");
 const cityName = document.getElementById("city");
 const buttonInput = document.querySelector("button");
 const coordonates = document.getElementById("gps");
+
+let cityTocall = "";//récupère la valeur de l'input plus tard. Ici pour pouvoir l'appeler ailleurs
+
+
+
+/*****************COORDONNEES************/
 
 const urlLyon =
   "https://nominatim.openstreetmap.org/search?q=Lyon&format=json&addressdetails=1&limit=1";
@@ -11,11 +17,14 @@ const urlParis =
   "https://nominatim.openstreetmap.org/search?q=Paris&format=json&addressdetails=1&limit=1";
 
 //relier l'input à l'appel api
-const urlInput = `https://nominatim.openstreetmap.org/search?q=${cityInput.value}&format=json&addressdetails=1&limit=1`; //Ne fonctionne pas pour le moment
+// const urlInput = `https://nominatim.openstreetmap.org/search?q=${cityTocall}&format=json&addressdetails=1&limit=1`; //Ne fonctionne pas pour le moment
 
-async function fetchCoordinates(town) {
+
+async function fetchCoordinates(url) {
   try {
-    const response = await fetch(town);
+    cityTocall = cityInput.value;
+
+    const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${cityTocall}&format=json&addressdetails=1&limit=1`);
     const townInput = await response.json();
 
     cityName.innerHTML = townInput[0].name; //pas obligatoire mais là pour le moment
@@ -26,13 +35,26 @@ async function fetchCoordinates(town) {
     console.log("Latitude : ", townInput[0].lat); // verifier navigation dans le tableau
     console.log(coordonates); //fonctionne aussi (moche)
   } catch (error) {
-    console.error("Failed to catch error: ", error);
+    console.error("Failed to catch data: ", error);
   }
 }
 
 fetchCoordinates(urlLyon); //là pour faire un vrai appel api pendant que je tente de lier l'input à l'appel
 
+/*****************METEO************/
+async function fetchWeather(latitude, longitude) {
+    try {
+        const response = await fetch(url);
+        const meteo = await response.json();
+    
+    } catch (error) {
+        console.error("Failed to catch weather data: ", error);
+      }
+}
+
+/*****************EVENTLISTENER************/
 buttonInput.addEventListener("click", () => {
-  cityName.innerHTML = cityInput.value; //test pour voir si la ville change de nom au clic
-  fetchCoordinates(urlInput);
+//   cityName.innerHTML = cityTocall; //la ville change de nom au clic
+    console.log("City to call : ", cityTocall);
+    fetchCoordinates();
 });
